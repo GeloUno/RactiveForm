998dedcb6798d3ba5a9f465c3198b466b2347d6c
GeloUno <geloke59@gmail.com>
2018-09-25 15:56:34 +0200

    ReactivForm
    module.ts add RactiveFormModule

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { WritePropExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-forms',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-
-  constructor() { }
+  mess: FormGroup;
+  messageReactive = new ReactiveMessage;
+  optionFramework = ['Angular', 'React', 'JavaScript', 'Java', 'C#'];
+  constructor() {}
 
   ngOnInit() {
+    this.mess = new FormGroup({
+      topic: new FormControl(null, Validators.required),
+      message: new FormControl(null),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      nick: new FormControl(null, Validators.required),
+      optionSelect: new FormControl(this.optionFramework[0])
+    });
   }
-
+  onSubmit(a) {
+    console.log(this.mess);
+   console.log(a);
+    this.messageReactive.topic = this.mess.value.topic;
+    this.messageReactive.message = this.mess.value.message;
+    this.messageReactive.email = this.mess.value.email;
+    this.messageReactive.nick = this.mess.value.nick;
+    this.messageReactive.optionSelect = this.mess.value.optionSelect;
+   console.log(this.messageReactive);
+  }
+}
+export class ReactiveMessage {
+  constructor(
+    public topic?: string,
+    public message?: string,
+    public email?: string,
+    public nick?: string,
+    public optionSelect?: string
+  ) {}
 }
