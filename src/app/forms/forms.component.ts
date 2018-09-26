8f030d0bc558e1235854ccd822bd656fb27e9f39
GeloUno <geloke59@gmail.com>
2018-09-26 02:54:17 +0200

    Form add radio buton check button imput more. Add Validators

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  NgForm,
+  FormArray
+} from '@angular/forms';
 import { WritePropExpr } from '@angular/compiler';
 
 @Component({
@@ -8,29 +14,37 @@ import { WritePropExpr } from '@angular/compiler';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
+  @ViewChild('form')
+  form: NgForm;
   mess: FormGroup;
-  messageReactive = new ReactiveMessage;
+  messageReactive = new ReactiveMessage();
   optionFramework = ['Angular', 'React', 'JavaScript', 'Java', 'C#'];
   constructor() {}
 
   ngOnInit() {
     this.mess = new FormGroup({
       topic: new FormControl(null, Validators.required),
-      message: new FormControl(null),
+      message: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
       nick: new FormControl(null, Validators.required),
-      optionSelect: new FormControl(this.optionFramework[0])
+      optionSelect: new FormControl(null, Validators.required), // this.optionFramework[0]
+      more: new FormArray([new FormControl(null), new FormControl(null)])
     });
   }
   onSubmit(a) {
     console.log(this.mess);
-   console.log(a);
+    console.log(a);
     this.messageReactive.topic = this.mess.value.topic;
     this.messageReactive.message = this.mess.value.message;
     this.messageReactive.email = this.mess.value.email;
     this.messageReactive.nick = this.mess.value.nick;
     this.messageReactive.optionSelect = this.mess.value.optionSelect;
-   console.log(this.messageReactive);
+    console.log(this.messageReactive);
+    // this.form.reset();
+  }
+  addmore() {
+    const nextMore = <FormArray>this.mess.get('more');
+    nextMore.push(new FormControl(null));
   }
 }
 export class ReactiveMessage {
